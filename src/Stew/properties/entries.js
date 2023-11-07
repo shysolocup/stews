e52d2a1f4a05b4f7e08ee36065b5cf3aac34e677
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-07 16:50:00 -0600

    Update entries.js

--- src/Stew/properties/entries.js
+++ src/Stew/properties/entries.js
@@ -2,8 +2,15 @@ const Stew = require('../index.js');
 
 
 function StewEntries() {
-	return this.insides.entries();
+	if (this.type == "pair") return Array.from(this.insides.entries());
+        else if (this.type == "list") {
+            let thing = Array.from(this.insides.entries());
+            thing.forEach( (value, index) => {
+                thing[index][0] = index.toString();
+            });
+            return thing;
+        }
 }
 
 
-Soup.newP("entries", SoupEntries);
+Soup.newP("entries", StewEntries);
