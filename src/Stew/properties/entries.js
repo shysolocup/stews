3663e0521640208cef2d42124c9b351f0792224e
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-07 16:50:19 -0600

    Update entries.js

--- src/Stew/properties/entries.js
+++ src/Stew/properties/entries.js
@@ -3,13 +3,13 @@ const Stew = require('../index.js');
 
 function StewEntries() {
 	if (this.type == "pair") return Array.from(this.insides.entries());
-        else if (this.type == "list") {
-            let thing = Array.from(this.insides.entries());
-            thing.forEach( (value, index) => {
-                thing[index][0] = index.toString();
-            });
-            return thing;
-        }
+	else if (this.type == "list") {
+		let thing = Array.from(this.insides.entries());
+		thing.forEach( (value, index) => {
+			thing[index][0] = index.toString();
+		});
+		return thing;
+	}
 }
 
 
