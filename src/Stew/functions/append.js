b8fbc6ace29afef5958b0bf85b9f6a0727f77ed6
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-07 17:12:15 -0600

    Create append.js

---
+++ src/Stew/functions/append.js
@@ -0,0 +1,28 @@
+const Stew = require('../index.js');
+
+
+function StewAppend(index, key, value=null) {
+	var stuff;
+  if (this.type == "list") {
+      stuff = new Stew(Array);
+      this.forEach( (v, i) => {
+          if (i == index) stuff.push(key);
+          stuff.push(v);
+      });
+
+      this.insides = stuff.pour(Set);
+  }
+  else if (this.type == "pair") {
+      stuff = new Stew(Object);
+      this.forEach( (k, v, i) => {
+          if (i == index) stuff.push(key, value);
+          stuff.push(k, v);
+      });
+
+      this.insides = stuff.pour(Map);
+  }
+}
+
+
+Stew.newF("append", StewAppend);
+Stew.newF("insert", StewAppend);
