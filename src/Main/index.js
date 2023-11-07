2221282495ce6b41d686d82bbfac1952824d364b
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-07 15:44:09 -0600

    Update index.js

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,6 +1,27 @@
+/*
+	# Stews #
+	:: created by @paigeroid/@nuttmegg using ::
+	- aepl: https://github.com/paigeroid/aepl
+  
+	## LINKS ##
+	- https://github.com/paigeroid/stews
+	- https://npmjs.com/package/stews
+
+ 	## PARTS ##
+  - https://npmjs.com/package/@stews/soup
+	- https://npmjs.com/package/@stews/stew
+ 	- https://npmjs.com/package/@stews/noodle
+
+*/
+
+
+
+// imports
 const Stew = require('@stews/stew');
 const Soup = require('@stews/soup');
 const Noodle = require('@stews/noodle');
 
 
+
+// exports
 module.exports = { Stew, Soup, Noodle };
