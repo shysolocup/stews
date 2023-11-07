7d3ba073217abdb69ceb4b778259d586f44f1457
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-07 12:46:38 -0600

    Create endsWith.js

---
+++ src/Soup/functions/endsWith.js
@@ -0,0 +1,16 @@
+const Soup = require('../index.js');
+
+
+function SoupEndsWith(/**/) {
+	let args = Array.from(arguments);
+	if (args[0] instanceof Array) args = args[0];
+	let stuff = this.join("");
+
+	for (let i = 0; i < args.length; i++) {
+		if (stuff.endsWith(args[i])) return true;
+	}
+	return false;
+}
+
+
+Soup.newF("endsWith", SoupEndsWith);
