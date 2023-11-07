5c361faf3471b9206c808acab621a67a8abb56ab
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-07 15:59:27 -0600

    Create get.js

---
+++ src/Stew/construct/__proxy/data/get.js
@@ -0,0 +1,18 @@
+const prox = require('../index.js');
+const Stew = require('../../../index.js');
+
+
+prox.get = (target, prop) => {
+    if (Object.getOwnPropertyNames(Stew.prototype).includes(prop) || target[prop]) { // if it's a function or main thing
+        return target[prop];
+    }
+    else if (Number(prop)+1 && Number(prop) <= target.length-1) { // if it's a number
+        return target.values[Number(prop)];
+    }
+    else if (typeof prop == "string") { // if it's string
+        return target.get(prop);
+    }
+    else {
+        return false;
+    }
+};
