eb3aa53612bf6fbfbaa4400857aabd99b83151ca
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-07 17:32:16 -0600

    Update clear.js

--- src/Stew/functions/clear.js
+++ src/Stew/functions/clear.js
@@ -3,7 +3,7 @@ const Stew = require('../index.js');
 
 function StewClear() {
 	let copy = this.copy();
-	copy.insides = (this.type=="pair") ? new Map() : new Soup();
+	copy.insides = (this.type=="pair") ? new Map() : new Set();
 	return copy;
 }
 
