1e55a7cd8c5ad26ddc10a936a1204f8011ad4148
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-07 15:37:03 -0600

    Update slice.js

--- src/Soup/functions/slice.js
+++ src/Soup/functions/slice.js
@@ -1,38 +1,14 @@
 const Soup = require('../index.js');
 
 
-function SoupShuffle() {
-	// thanks to this https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-
-	let copy = this.copy()
-	let ci = copy.length-1, ri;
-
-	
-  	while (ci > 0) {
-    	ri = Math.floor(Math.random() * ci);
-
-		
-		if (copy.type == "list") {
-			let [a, b] = [ copy.get(ci), copy.get(ri) ];
-			copy.set(ri, a);
-			copy.set(ci, b);
-		}
-
-		else if (copy.type == "pair") {
-			let [a, b] = [ copy.entries[ci], copy.entries[ri] ];
-
-			let thing = copy.entries;
-
-			[ thing[ri], thing[ci] ] = [ a, b ];
-
-			copy.insides = Object.fromEntries(thing);
-		}
-
-		ci--;
-  }
-
-  return copy;
+function SoupSlice(start, end) {
+	if (this.type == "pair") {
+		return new Soup( Object.fromEntries( this.entries.slice(start, end)) );
+	}
+	else if (this.type == "list") {
+		return new Soup(this.insides.slice(start, end));
+	}
 }
 
 
-Soup.newF("shuffle", SoupShuffle);
+Soup.newF("slice", SoupSlice);
