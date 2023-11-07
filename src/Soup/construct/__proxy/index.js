a50d32e314193ad53b9fb458584e2a7e9f615539
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-07 15:09:10 -0600

    Update index.js

--- src/Soup/construct/__proxy/index.js
+++ src/Soup/construct/__proxy/index.js
@@ -5,7 +5,7 @@ module.exports = {};
 
 
 // compiling properties
-let prox_dir = require('./_funkydir');
+let prox_dir = require('./data/_funkydir');
 let prox = fs.readdirSync(prox_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));  
 prox.forEach( (file) => {
 	require(`./${file}`);
