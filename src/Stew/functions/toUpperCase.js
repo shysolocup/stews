b4fcc8c25a63420984476343b998ee93a1a9d10c
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-07 18:53:58 -0600

    Create toUpperCase.js

---
+++ src/Stew/functions/toUpperCase.js
@@ -0,0 +1,37 @@
+const Stew = require('../index.js');
+
+
+function StewToUpperCase(/**/) {
+	let args = Array.from(arguments);
+		if (args[0] instanceof Array) args = args[0];
+		
+        var stuff = this.copy();
+		
+        if (stuff.type == "list") {
+			if (args.length > 0) {
+				args.forEach( (i) => {
+					if (typeof stuff[i] == "string") stuff.set(i, stuff[i].toUpperCase());
+				});
+			}
+            else stuff.forEach( (value, index) => {
+                if (typeof value == "string") stuff.set(index, value.toUpperCase());
+            });
+        }
+        else if (stuff.type == "pair") {
+			if (args.length > 0) {
+				args.forEach( (i) => {
+					if (typeof stuff.values[i] == "string") stuff.set(i, stuff.values[i].toUpperCase());
+					if (typeof stuff.keys[i] == "string") stuff.rename(i, stuff.keys[i].toUpperCase());
+				});
+			}
+            else stuff.forEach( (key, value, index) => {
+                if (typeof value == "string") stuff.set(index, value.toUpperCase());
+                if (typeof key == "string") stuff.rename(index, key.toUpperCase());
+            });
+        }
+        return stuff;
+}
+
+
+Stew.newF("upper", StewToUpperCase);
+Stew.newF("toUpperCase", StewToUpperCase);
