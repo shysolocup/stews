25c0c08d8b466766885050669033e6490aabb119
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-08 10:42:52 -0600

    Update push.js

--- src/Noodle/functions/push.js
+++ src/Noodle/functions/push.js
@@ -2,7 +2,7 @@ const Noodle = require('../index.js');
 const Soup = require('@stews/soup');
 
 
-function NoodlePush(entry, value=null) {
+function NoodlePush(value=null) {
 	let stuff = Soup.from(this.content);
 		stuff.push(value);
 		return this.content = stuff.join("");
