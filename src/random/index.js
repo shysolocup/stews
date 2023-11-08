ba30eb9888493cb7617f8c1cbc10843d07458c97
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-08 13:09:24 -0600

    Update index.js

--- src/random/index.js
+++ src/random/index.js
@@ -1,8 +1,30 @@
+/*
+	# @stews/random #
+	:: created by @paigeroid/@nuttmegg using ::
+	- aepl: https://github.com/paigeroid/aepl
+  
+	## LINKS ##
+	- https://github.com/paigeroid/stews/tree/main/src/random
+	- https://npmjs.com/package/@stews/random
+
+ 	## MAIN ##
+	- https://github.com/paigeroid/stews
+	- https://npmjs.com/package/stews
+
+ 	## PARTS ##
+   	- https://npmjs.com/package/@stews/soup
+	- https://npmjs.com/package/@stews/stew
+ 	- https://npmjs.com/package/@stews/noodle
+  	- https://npmjs.com/package/@stews/random
+
+*/
+
+
 const cl = require('aepl');
 
 cl.init("random", class {
-  constructor(bind=null) {
-    if (bind) this.bind = bind;
+  constructor(binder=null) {
+    if (binder) this.binder = binder;
   }
 });
 
