48bd8b85ae653affea8cf2cb84a32b79a5c728ab
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-08 11:01:46 -0600

    Create includes.js

---
+++ src/Noodle/functions/includes.js
@@ -0,0 +1,13 @@
+const Noodle = require('../index.js');
+
+
+function NoodleIncludes(/**/) {
+	let args = Array.from(arguments);
+		if (args.length == 1 && args[0] instanceof Array) args = args[0];
+
+        return args.some( (v) => { return this.content.includes(v); });
+}
+
+
+Noodle.newF("contains", NoodleIncludes);
+Noodle.newF("has", NoodleIncludes);
