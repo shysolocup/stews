7de6e38fe468aa0f3f55bea0d97ae1b82f720039
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-07 18:39:42 -0600

    Create shuffle.js

---
+++ src/Stew/functions/shuffle.js
@@ -0,0 +1,38 @@
+const Stew = require('../index.js');
+
+
+function StewShuffle() {
+	// thanks to this https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+
+	let copy = this.copy()
+	let ci = copy.length-1, ri;
+
+	
+  	while (ci > 0) {
+    	ri = Math.floor(Math.random() * ci);
+
+		
+		if (copy.type == "list") {
+			let [a, b] = [ copy.get(ci), copy.get(ri) ];
+			copy.set(ri, a);
+			copy.set(ci, b);
+		}
+
+		else if (copy.type == "pair") {
+			let [a, b] = [ copy.entries[ci], copy.entries[ri] ];
+
+			let thing = copy.entries;
+
+			[ thing[ri], thing[ci] ] = [ a, b ];
+
+			copy.insides = new Map(thing);
+		}
+
+		ci--;
+  }
+
+  return copy;
+}
+
+
+Stew.newF("shuffle", StewShuffle);
