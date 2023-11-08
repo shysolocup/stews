d5f61c3df464cdea090ed5aca3ca0359463bde66
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-08 12:03:26 -0600

    Update and rename choice.js to int.js

---
+++ src/random/functions/int.js
@@ -0,0 +1,10 @@
+const random = require('../index.js');
+
+
+function RandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+
+random.newF("int", RandomInt);
+random.newF("number", RandomInt);
