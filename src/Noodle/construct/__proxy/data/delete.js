3cc76cfc49f773634e2835ccc08d7639f4a57af9
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-08 10:25:17 -0600

    Update delete.js

--- src/Noodle/construct/__proxy/data/delete.js
+++ src/Noodle/construct/__proxy/data/delete.js
@@ -1,12 +1,15 @@
 const prox = require('../index.js');
+const Soup = require('@stews/soup');
 
 
 prox.deleteProperty = (target, prop) => {
   if (Number(prop)+1 && Number(prop) <= target.length-1) { // if it is a number
-				target.delete(Number(prop));
-                return true;
-            }
-            else {
-                return false;
-            }
+		target.content = new Soup(target.content.split(""))
+	  	target.content.delete(Number(prop));
+	  	target.content = target.content.join("");
+		return true;
+	}
+	else {
+		return false;
+	}
 };
