8f3c72f4de38261dc24dd8003ccd4313098ec9a0
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-07 18:03:37 -0600

    Create toLowerCase.js

---
+++ src/Stew/functions/toLowerCase.js
@@ -0,0 +1,36 @@
+const Stew = require('../index.js');
+
+
+function StewToLowerCase() {
+	let args = Array.from(arguments);
+		if (args[0] instanceof Array) args = args[0];
+		
+        var stuff = this.copy();
+		
+        if (stuff.type == "list") {
+			if (args.length > 0) {
+				args.forEach( (i) => {
+					if (typeof stuff[i] == "string") stuff.set(i, stuff[i].toLowerCase());
+				});
+			}
+            else stuff.forEach( (value, index) => {
+                if (typeof value == "string") stuff.set(index, value.toLowerCase());
+            });
+        }
+        else if (stuff.type == "pair") {
+			if (args.length > 0) {
+				args.forEach( (i) => {
+					if (typeof stuff.values[i] == "string") stuff.set(i, stuff.values[i].toLowerCase());
+					if (typeof stuff.keys[i] == "string") stuff.rename(i, stuff.keys[i].toLowerCase());
+				});
+			}
+            else stuff.forEach( (key, value, index) => {
+                if (typeof value == "string") stuff.set(index, value.toLowerCase());
+                if (typeof key == "string") stuff.rename(index, key.toLowerCase());
+            });
+        }
+        return stuff;
+}
+
+
+Stew.newF("toLowerCase", StewLowerCase);
