48772744988e50a85cddd88e3fdce6f361af08c9
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-08 10:43:12 -0600

    Update set.js

--- src/Noodle/functions/set.js
+++ src/Noodle/functions/set.js
@@ -2,7 +2,7 @@ const Noodle = require('../index.js');
 const Soup = require('@stews/soup');
 
 
-function NoodleSet(entry, value=null) {
+function NoodleSet(index, set_to=null) {
 	let stuff = Soup.from(this.content);
         stuff.set(index, set_to);
 		return this.content = stuff.join("");
