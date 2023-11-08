4716ffeefae11adf05b1a858ab17982115ab4dd6
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-08 13:40:58 -0600

    Update reverse.js

--- src/Noodle/functions/reverse.js
+++ src/Noodle/functions/reverse.js
@@ -3,7 +3,7 @@ const Noodle = require('../index.js');
 
 function NoodleReverse() {
   let copy = this.copy();
-  copy.content = copy.content.split("").reverse().join(""));
+  copy.content = copy.content.split("").reverse().join("");
   return copy;
 }
 
