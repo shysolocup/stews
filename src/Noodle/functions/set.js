c8ca4ddd5fc2845252ac7a5944911eb0ac94ba72
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-08 10:40:21 -0600

    Create set.js

---
+++ src/Noodle/functions/set.js
@@ -0,0 +1,12 @@
+const Noodle = require('../index.js');
+const Soup = require('@stews/soup');
+
+
+function NoodleSet(entry, value=null) {
+	let stuff = Soup.from(this.content);
+        stuff.set(index, set_to);
+		return this.content = stuff.join("");
+}
+
+
+Noodle.newF("set", NoodleSet);
