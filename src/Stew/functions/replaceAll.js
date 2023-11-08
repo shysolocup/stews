f32683bb817e5ca9e3b73451b284eeb0b1167673
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-07 18:33:46 -0600

    Create replaceAll.js

---
+++ src/Stew/functions/replaceAll.js
@@ -0,0 +1,25 @@
+const Stew = require('../index.js');
+
+
+function StewReplaceAll(entry, replaceWith) {
+	let thing = this.copy();
+        if (thing.type == "list") {
+            thing.forEach( (value, index) => {
+                var stuff = value; while (stuff.includes(entry)) stuff = stuff.replace(entry, replaceWith);
+                
+                if (value.includes(entry)) thing.set(index, stuff);
+            });
+        }
+        else if (thing.type == "pair") {
+            thing.forEach( (key, value, index) => {
+                var stuff = key; while (stuff.includes(entry)) stuff = stuff.replace(entry, replaceWith);
+                
+                if (key.includes(entry)) thing.rename(index, stuff);
+            });
+        }
+        
+        return thing;
+}
+
+
+Stew.newF("replaceAll", StewReplaceAll);
