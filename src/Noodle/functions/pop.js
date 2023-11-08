d1fd73a9178c3cee6979aa7b027b87d71d711fdd
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-08 10:50:11 -0600

    Create pop.js

---
+++ src/Noodle/functions/pop.js
@@ -0,0 +1,14 @@
+const Noodle = require('../index.js');
+const Soup = require('@stews/soup');
+
+
+function NoodlePop(offset=0) {
+	let index = (this.length-1)-offset;
+  let returns = { value: this.get(index), index: index };
+  this.delete(index);
+  return returns;
+}
+
+
+Noodle.newF("pop", NoodlePop);
+Noodle.newF("unpush", NoodlePop);
