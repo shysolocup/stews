06763837945d4a62adc365a1574b5d330d624b11
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-07 19:48:42 -0600

    Create delete.js

---
+++ src/Noodle/construct/__proxy/data/delete.js
@@ -0,0 +1,12 @@
+const prox = require('../index.js');
+
+
+prox.deleteProperty = (target, prop) => {
+  if (Number(prop)+1 && Number(prop) <= target.length-1) { // if it is a number
+				target.delete(Number(prop));
+                return true;
+            }
+            else {
+                return false;
+            }
+};
