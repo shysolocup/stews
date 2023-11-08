c0fa1c23c5d756447b1df08d6e22988b2ad30dce
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-07 19:51:54 -0600

    Update get.js

--- src/Soup/construct/__proxy/data/get.js
+++ src/Soup/construct/__proxy/data/get.js
@@ -3,16 +3,20 @@ const Soup = require('../../../index.js');
 
 
 prox.get = (target, prop) => {
-    if (Object.getOwnPropertyNames(Soup.prototype).includes(prop) || target[prop]) { // if it's a function or main thing
+    try {
+        if (Object.getOwnPropertyNames(Soup.prototype).includes(prop) || target[prop]) { // if it's a function or main thing
+            return target[prop];
+        }
+        else if (Number(prop)+1 && Number(prop) <= target.length-1) { // if it's a number
+            return target.values[Number(prop)];
+        }
+        else if (typeof prop == "string") { // if it's string
+            return target.get(prop);
+        }
+        else {
+            return false;
+        }
+    } catch(e) {
         return target[prop];
-    }
-    else if (Number(prop)+1 && Number(prop) <= target.length-1) { // if it's a number
-        return target.values[Number(prop)];
-    }
-    else if (typeof prop == "string") { // if it's string
-        return target.get(prop);
-    }
-    else {
-        return false;
     }
 };
