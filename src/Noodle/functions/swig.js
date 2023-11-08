ea73ef49ff4ad36e3477155cc943c43799e11e15
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-08 11:09:32 -0600

    Create swig.js

---
+++ src/Noodle/functions/swig.js
@@ -0,0 +1,12 @@
+const Noodle = require('../index.js');
+const Soup = require('@stews/soup');
+
+
+function NoodleSwig(func) {
+	let stuff = Soup.from(this.copy(), "");
+		return stuff.swig(func);
+}
+
+
+Noodle.newF("swig", NoodleSwig);
+Noodle.newF("some", NoodleSwig
