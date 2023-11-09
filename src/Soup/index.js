da9cca24f067a9efb3d08fc9f8e7a9a459508527
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-09 16:38:25 -0600

    Update index.js

--- src/Soup/index.js
+++ src/Soup/index.js
@@ -48,54 +48,6 @@ cl.init("Soup", class {
 		// creates the proxy
         return new Proxy(this, __proxy);
 	}
-
-	
-
-    // properties
-    get properties() {
-        let proto = this.constructor.prototype;
-        let names = Object.getOwnPropertyNames(proto);
-        let info = [];
-        let methods = {}
-
-        Object.getOwnPropertyNames(this).forEach( (name) => {
-            methods[name] = (Object.getOwnPropertyDescriptors(this)[name].value) ? Object.getOwnPropertyDescriptors(this)[name].value : this[name];
-            names.unshift(name);
-        });
-
-        Object.getOwnPropertyNames(proto).forEach( (name) => {
-            if (Object.getOwnPropertyDescriptors(proto)[name].value) {
-                methods[name] = Object.getOwnPropertyDescriptors(proto)[name].value;
-            }
-            else if (name == "props" || name == "properties") {
-                methods[name] = Object.getOwnPropertyDescriptors(proto)[name].get
-            }
-            else if ( Soup.primaryInfo.includes(name) ) {
-                let entries = [];
-
-                Object.entries(methods).forEach( (entry, index) => {
-                    if (entry[0] == "constructor") entries.push( [name, this[name]] ); entries.push(entry);
-                });
-
-                methods = Object.fromEntries(entries);
-            }
-            else {
-                methods[name] = this[name];
-            }
-        });
-
-        info = Object.fromEntries(Object.entries(methods).filter( (entry, index) => {
-            return Soup.primaryInfo.includes(entry[0]);
-        }));
-
-        return {
-            names: names,
-            descriptors: Object.getOwnPropertyDescriptors(proto),
-            methods: methods,
-            info: info
-        }
-    }
-    get props() { return this.properties; }
 });
 
 
