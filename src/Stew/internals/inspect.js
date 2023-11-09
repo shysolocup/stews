739140e1634c5266c472a8b8bc50eca3fd870090
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-09 16:24:17 -0600

    Update inspect.js

--- src/Stew/internals/inspect.js
+++ src/Stew/internals/inspect.js
@@ -3,28 +3,7 @@ const util = require('util');
 
 
 Stew.prototype[util.inspect.custom] = function(depth, opts) {
-  let data;
-  if (this.type == "list") {
-    data = `[ ${this.map( v => `${
-      (typeof v == "string") ? `"${v}"` :
-      (typeof v == "object") ? `[${v.constructor.name}]` :
-      v
-    }`).join(", ")} ]`;
-  }
-    
-  else if (this.type == "pair") {
-    data = `{ ${this.entries.map( v => `${
-
-            (typeof v[0] == "string" && v[0].includes(" ")) ? `"${v[0]}"` : v[0]
-
-        }: ${
-
-        (typeof v[1] == "object") ? `[${v[1].constructor.name}]` : 
-        (typeof v[1] == "string")?  `"${v[1]}"`
-        : v[1]
-
-    }`).flat().join(", ")} }`
-  }
+  opts.colors = true;
   
-  return `Stew(${this.length}) ${data}`;
+  return `Stew(${this.length}) ${util.inspect(this.insides, { colors: true })}`;
 }
