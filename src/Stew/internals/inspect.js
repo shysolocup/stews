082b9bd9edc9521751f3e5b7ab32d455728e32ea
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-09 11:44:00 -0600

    Update inspect.js

--- src/Stew/internals/inspect.js
+++ src/Stew/internals/inspect.js
@@ -2,6 +2,6 @@ const Stew = require('../index.js');
 const util = require('util');
 
 
-Stew.newF([util.inspect.custom], function(depth, opts) {
+Stew.prototype[util.inspect.custom] = function(depth, opts) {
   return `Stew(${this.length}) ${this.stringify(null, 4)}`;
-});
+}
