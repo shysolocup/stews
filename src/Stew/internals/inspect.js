735e629d9c345523f3e04161ef34bf96b4e77bd1
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-09 16:33:07 -0600

    Update inspect.js

--- src/Stew/internals/inspect.js
+++ src/Stew/internals/inspect.js
@@ -5,5 +5,5 @@ const util = require('util');
 Stew.prototype[util.inspect.custom] = function(depth, opts) {
   opts.colors = true;
   
-  return `Stew(${this.length}) ${util.inspect(this.insides, { colors: true })}`;
+  return `Stew(${this.length}) ${util.inspect(this.pour(), { colors: true })}`;
 }
