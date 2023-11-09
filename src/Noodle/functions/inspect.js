645ada8c577f83109379bcd11b2da370b0bc2d23
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-09 11:24:39 -0600

    Delete src/Noodle/functions/inspect.js

--- src/Noodle/functions/inspect.js
+++
@@ -1,9 +0,0 @@
-const Noodle = require('../index.js');
-
-
-function NoodleInspect(depth, opts) {
-  return `Noodle(${this.length}) { ${this.contents} }`;
-}
-
-
-Noodle.newF("inspect", NoodleInspect);
