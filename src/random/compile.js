dfb85ea2c0dc5c075d87fac76083e928543cb1b9
paige <88659700+paigeroid@users.noreply.github.com>
2023-11-09 18:57:30 -0600

    Update compile.js

--- src/random/compile.js
+++ src/random/compile.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 
 
 module.exports = (d) => {
-    let dir = require(`./${d}/_funkydir`);
+    let dir = require(`${d}/_funkydir`);
     let files = fs.readdirSync(dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));  
     files.forEach( (file) => {
-        require(`./${d}/${file}`);
+        require(`${d}/${file}`);
     });
 };
