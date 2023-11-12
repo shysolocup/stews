d098dd22a5a4f0dd1cb45b382772f1f02e079382
paige <88659700+paishee@users.noreply.github.com>
2023-11-11 22:52:40 -0600

    Update index.js

--- src/random/index.js
+++ src/random/index.js
@@ -1,14 +1,14 @@
 /*
 	# @stews/random #
-	:: created by @paigeroid/@nuttmegg using ::
-	- aepl: https://github.com/paigeroid/aepl
+	:: created by @paishee using ::
+	- aepl: https://github.com/paishee/aepl
   
 	## LINKS ##
-	- https://github.com/paigeroid/stews/tree/main/src/random
+	- https://github.com/paishee/stews/tree/main/src/random
 	- https://npmjs.com/package/@stews/random
 
  	## MAIN ##
-	- https://github.com/paigeroid/stews
+	- https://github.com/paishee/stews
 	- https://npmjs.com/package/stews
 
  	## PARTS ##
