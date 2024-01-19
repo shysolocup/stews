6fba0f6572ffa6b830d96249b6dc36d0e458e22b
paige <88659700+paishee@users.noreply.github.com>
2024-01-19 08:56:47 -0600

    Update compile.js

--- src/Noodle/compile.js
+++ src/Noodle/compile.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 
 
 module.exports = (d) => {
-    let dir = require(`${d}/_funkydir`);
+    let dir = `${__dirname}${d}`
     let files = fs.readdirSync(dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));  
     files.forEach( (file) => {
-        require(`${d}/${file}`);
+        require(dir);
     });
 };
