9ae798b605698ab578363d0fcd48079efe0ae652
paige <88659700+paishee@users.noreply.github.com>
2024-01-23 07:54:43 -0600

    Update compile.js

--- src/Soup/compile.js
+++ src/Soup/compile.js
@@ -5,6 +5,6 @@ module.exports = (d) => {
     let dir = `${__dirname}${d}`
     let files = fs.readdirSync(dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));  
     files.forEach( (file) => {
-        require(dir);
+        require(`${dir}/${file}`);
     });
 };
