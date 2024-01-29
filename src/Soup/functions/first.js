3d02c28f00c8bd6854452568795aaa501d4e319e
paige <88659700+paishee@users.noreply.github.com>
2024-01-29 08:45:08 -0600

    Update first.js

--- src/Soup/functions/first.js
+++ src/Soup/functions/first.js
@@ -3,7 +3,7 @@ const Soup = require('../index.js');
 
 function SoupFirst(offset=0) {
 	let entry = this.entries[0+offset];
-	return (this.type == "pair") ? {key: entry[0], value: entry[1], index: 0} : entry[1];
+	return entry[1];
 }
 
 
