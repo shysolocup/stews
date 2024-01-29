2d56f01021907f4ff2e15d734872f7b8e66a1122
paige <88659700+paishee@users.noreply.github.com>
2024-01-29 08:48:41 -0600

    Update last.js

--- src/Soup/functions/last.js
+++ src/Soup/functions/last.js
@@ -3,7 +3,7 @@ const Soup = require('../index.js');
 
 function SoupLast(offset=0) {
 	let entry = this.entries[(this.length-1)-offset];
-	return (this.type == "pair") ? {key: entry[0], value: entry[1], index: this.length-1} : entry[1];
+	return entry[1]
 }
 
 
