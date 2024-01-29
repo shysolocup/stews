9561cc6b1bfbaed59559358abb994a54775b9664
paige <88659700+paishee@users.noreply.github.com>
2024-01-29 08:56:50 -0600

    Update first.js

--- src/Stew/functions/first.js
+++ src/Stew/functions/first.js
@@ -3,7 +3,7 @@ const Stew = require('../index.js');
 
 function StewFirst(offset=0) {
 	let entry = this.entries[0+offset];
-	return (this.type == "pair") ? {key: entry[0], value: entry[1], index: 0} : entry[1];
+	return entry[1];
 }
 
 
