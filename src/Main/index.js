a128e1b619c1311c700c8879d4f86271642c2471
paige <88659700+paishee@users.noreply.github.com>
2024-02-07 08:40:45 -0600

    Update index.js

--- src/Main/index.js
+++ src/Main/index.js
@@ -12,6 +12,7 @@
 	- https://npmjs.com/package/@stews/stew
  	- https://npmjs.com/package/@stews/noodle
   	- https://npmjs.com/package/@stews/random
+   	- https://npmjs.com/package/@stews/bean
 
 */
 
@@ -19,12 +20,13 @@
 
 // imports
 
-let [ Stew, Soup, Noodle, random ]=[];
+let [ Stew, Soup, Noodle, Bean, random ]=[];
 try { Stew = require('../Stew'); } catch(e) { Stew = require('@stews/stew'); }
 try { Soup = require('../Soup'); } catch(e) { Soup = require('@stews/soup'); }
 try { Noodle = require('../Noodle'); } catch(e) { Noodle = require('@stews/noodle'); }
+try { Bean = require('../Bean'); } catch(e) { Noodle = require('@stews/bean'); }
 try { random = new (require('../random')); } catch(e) { random = new (require('@stews/random')); }
 
 
 // exports
-module.exports = { Stew, Soup, Noodle, random };
+module.exports = { Stew, Soup, Noodle, Bean, random };
