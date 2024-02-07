860a307a2efecedf596c93e4f5602f50f4d7736a
paige <88659700+paishee@users.noreply.github.com>
2024-02-07 09:02:22 -0600

    Create iterator.js

---
+++ src/Bean/internals/iterator.js
@@ -0,0 +1,19 @@
+const Bean = require('../index.js');
+
+
+Bean.newF(Symbol.iterator, function() {
+    var stuff = this;
+        return {
+            current: 0,
+            last: stuff.length-1,
+
+            next() {
+                if (this.current <= this.last) {
+                    let data = stuff.get(this.current++);
+                    return { done: false, value: data };
+                } else {
+                    return { done: true };
+                }
+            }
+        };
+});
