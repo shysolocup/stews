abdd4e44659cb5a6273e78e64a746852f2d760fd
paige <88659700+paishee@users.noreply.github.com>
2024-02-07 11:35:41 -0600

    Create equals.js

---
+++ src/Bean/functions/equals.js
@@ -0,0 +1,16 @@
+const Bean = require('../index.js');
+
+
+function BeanEquals(...args) {
+        if (args[0] instanceof Array) args = args[0];
+        let stuff = this.ints;
+
+        for (let i = 0; i < args.length; i++) {
+            if (stuff == args[i]) return true;
+        }
+        return false;
+}
+
+
+Bean.newF("equals", BeanEquals);
+Bean.newF("equalTo", BeanEquals);
