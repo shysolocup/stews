1dcec31953744d7b556350b5209b53c3c1da23b7
paige <88659700+paishee@users.noreply.github.com>
2024-02-07 11:11:40 -0600

    Create timeify.js

---
+++ src/Bean/functions/timeify.js
@@ -0,0 +1,20 @@
+const Bean = require('../index.js');
+
+
+function BeanTimeify(type) {
+	let t = this.content.toString().split("");
+	let thing = type;
+
+	if ( ["s", "sec", "second", "seconds"].includes(type) ) { return parseFloat(t.join("")); }
+	else if ( ["ms", "msec", "millisecond", "milliseconds"].includes(type) ) { return parseFloat(t.join(""))*1000; }
+	else if ( ["m", "min", "minute", "minutes"].includes(type) ) { return parseFloat(t.join(""))*60; }
+	else if ( ["h", "hr", "hour", "hours"].includes(type) ) { return parseFloat(t.join(""))*60*60; }
+	else if ( ["d", "dy", "day", "days" ].includes(type) ) { return parseFloat(t.join(""))*60*60*24; }
+	else if ( ["w", "wk", "week", "weeks"].includes(type) ) { return parseFloat(t.join(""))*60*60*24*7; }
+	else if ( ["y", "yr", "year", "years"].includes(type) ) { return parseFloat(t.join(""))*60*60*24*365; }
+	else return this.content;
+}
+
+
+Bean.newF("timeify", BeanTimeify);
+Bean.newF("timify", BeanTimeify);
