ea0fc8a5b791a27472011dc5b2667872044d3427
paige <88659700+paishee@users.noreply.github.com>
2024-02-07 11:19:17 -0600

    Update toPrimitive.js

--- src/Bean/internals/toPrimitive.js
+++ src/Bean/internals/toPrimitive.js
@@ -2,11 +2,11 @@ const Bean = require('../index.js');
 
 
 Bean.newF(Symbol.toPrimitive, function(hint) {
-    if (hint === "string" || hint == "default") {
+    if (hint === "string") {
             return this.toString();
         }
-        else if (hint == "number") {
-            return Number(this.content);
+        else if (hint == "number" || hint == "default") {
+            return this.content;
 		}
         return this;
 });
