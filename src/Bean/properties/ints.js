ad682ec02a1da049cfbbc6fb432f26e46f4640b9
paige <88659700+paishee@users.noreply.github.com>
2024-02-07 11:54:59 -0600

    Update ints.js

--- src/Bean/properties/ints.js
+++ src/Bean/properties/ints.js
@@ -2,8 +2,15 @@ const Bean = require('../index.js');
 
 
 function BeanInts() {
-	let seps = this.content.toString().split(".");
-	seps = seps.map( sep => sep.split("").map( i => (Number(i)+1) ? Number(i) : i );
+	let seps = this.content.toString()
+	if (seps.includes(".")) {
+		seps = seps.split(".");
+		seps = seps.map( sep => sep.split("").map( i => (Number(i)+1) ? Number(i) : i );
+	}
+	else {	
+		seps = seps.split("");
+		seps = seps.map( i => (Number(i)+1) ? Number(i) : i );
+	}
 
 	return seps;
 }
