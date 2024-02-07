72903fd5a09530a84676e949d717686c06ee8c4f
paige <88659700+paishee@users.noreply.github.com>
2024-02-07 10:37:47 -0600

    Create index.js

---
+++ src/Bean/construct/__proxy/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+module.exports = {};
+
+// compiling properties
+let prox_dir = `${__dirname}/data`;
+let prox = fs.readdirSync(prox_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts') )) );
+prox.forEach( file => {
+    require(`./data/${file}`);
+});
