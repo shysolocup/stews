2fd51fc3ea83be8fc72cc98c7be7fb7df63ad0c7
paige <88659700+paishee@users.noreply.github.com>
2024-02-07 08:56:40 -0600

    Update startsWith.js

--- src/Soup/functions/startsWith.js
+++ src/Soup/functions/startsWith.js
@@ -1,8 +1,7 @@
 const Soup = require('../index.js');
 
 
-function SoupStartsWith(/**/) {
-	let args = Array.from(arguments);
+function SoupStartsWith(...args) {
 	if (args[0] instanceof Array) args = args[0];
 	let stuff = this.join("");
 
