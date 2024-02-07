a270a7e72ba193324af07a407f92e86912b803d2
paige <88659700+paishee@users.noreply.github.com>
2024-02-07 12:05:25 -0600

    Update append.js

--- src/Bean/functions/append.js
+++ src/Bean/functions/append.js
@@ -3,15 +3,29 @@ const Soup = require('@stews/soup');
 
 
 function BeanAppend(index, set_to=null) {
-	var stuff;
-    stuff = new Soup(Array);
-    
-    this.ints.flat().forEach( (v, i) => {
-        if (i == index) stuff.push(set_to);
-        stuff.push(v);
-    });
+	var stuff = new Soup(Array);
+	var stuff2 = new Soup(Array);
 
-    this.content = parseFloat(stuff.join(""));
+	if (this.ints[0] instanceof Array) {
+		this.ints[0].forEach( (v, i) => {
+	        if (i == index) stuff.push(set_to);
+	        stuff.push(v);
+		});
+		this.ints[1].forEach( (v, i) => {
+	        if (i+(this.ints[0].length) == index) stuff2.push(set_to);
+	        stuff2.push(v);
+		});
+		
+		this.content = parseFloat(`${stuff.join("")}.${stuff2.join("")}`);
+	}
+	else {
+		this.ints.forEach( (v, i) => {
+	        if (i == index) stuff.push(set_to);
+	        stuff.push(v);
+		});
+		
+		this.content = parseFloat(stuff.join(""));
+	}
 
     return this;
 }
