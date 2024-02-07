5697923de9262417b340f6225d29bebe997070b7
paige <88659700+paishee@users.noreply.github.com>
2024-02-07 10:48:33 -0600

    Create get.js

---
+++ src/Bean/construct/__proxy/data/get.js
@@ -0,0 +1,17 @@
+const prox = require('../index.js');
+const Bean = require('../../../index.js');
+
+
+prox.get = (target, prop) => {
+    try {
+        if (Object.getOwnPropertyNames(Bean.prototype).includes(prop) || target[prop]) {
+            return target[prop]
+        }
+        else {
+            return target.get(prop);
+        }
+    }
+    catch() {
+        return target[prop];
+    }
+}
