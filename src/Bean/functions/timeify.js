f7c872c32f4b4bc2bd9da00e66f1bae111774851
paige <88659700+paishee@users.noreply.github.com>
2024-02-07 11:16:35 -0600

    Update timeify.js

--- src/Bean/functions/timeify.js
+++ src/Bean/functions/timeify.js
@@ -5,14 +5,14 @@ function BeanTimeify(type) {
 	let t = this.content.toString().split("");
 	let thing = type;
 
-	if ( ["s", "sec", "second", "seconds"].includes(type) ) { return parseFloat(t.join("")); }
-	else if ( ["ms", "msec", "millisecond", "milliseconds"].includes(type) ) { return parseFloat(t.join(""))*1000; }
-	else if ( ["m", "min", "minute", "minutes"].includes(type) ) { return parseFloat(t.join(""))*60; }
-	else if ( ["h", "hr", "hour", "hours"].includes(type) ) { return parseFloat(t.join(""))*60*60; }
-	else if ( ["d", "dy", "day", "days" ].includes(type) ) { return parseFloat(t.join(""))*60*60*24; }
-	else if ( ["w", "wk", "week", "weeks"].includes(type) ) { return parseFloat(t.join(""))*60*60*24*7; }
-	else if ( ["y", "yr", "year", "years"].includes(type) ) { return parseFloat(t.join(""))*60*60*24*365; }
-	else return this.content;
+	if ( ["s", "sec", "second", "seconds"].includes(type) ) { return Bean.from(parseFloat(t.join(""))); }
+	else if ( ["ms", "msec", "millisecond", "milliseconds"].includes(type) ) { return Bean.from(parseFloat(t.join(""))*1000); }
+	else if ( ["m", "min", "minute", "minutes"].includes(type) ) { return Bean.from(parseFloat(t.join(""))*60); }
+	else if ( ["h", "hr", "hour", "hours"].includes(type) ) { return Bean.from(parseFloat(t.join(""))*60*60); }
+	else if ( ["d", "dy", "day", "days" ].includes(type) ) { return Bean.from(parseFloat(t.join(""))*60*60*24); }
+	else if ( ["w", "wk", "week", "weeks"].includes(type) ) { return Bean.from(parseFloat(t.join(""))*60*60*24*7); }
+	else if ( ["y", "yr", "year", "years"].includes(type) ) { return Bean.from(parseFloat(t.join(""))*60*60*24*365); }
+	else return this;
 }
 
 
