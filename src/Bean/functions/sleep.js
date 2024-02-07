1e52886842523105e2f7ceec2faf5c5f3982daf3
paige <88659700+paishee@users.noreply.github.com>
2024-02-07 11:00:53 -0600

    Create sleep.js

---
+++ src/Bean/functions/sleep.js
@@ -0,0 +1,12 @@
+// return new Promise(resolve => setTimeout(resolve, this.parse(time)*1000));
+
+const Bean = require('../index.js');
+
+
+function BeanSleep() {
+  return new Promise(resolve => setTimeout(resolve, this.content*1000);
+}
+
+
+Bean.newF("sleep", BeanSleep);
+Bean.newF("wait", BeanSleep);
