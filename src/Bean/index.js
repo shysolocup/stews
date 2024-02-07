8a5ce35616955c8d78e4f5e263b34205ddb83f5b
paige <88659700+paishee@users.noreply.github.com>
2024-02-07 08:36:54 -0600

    Create index.js

---
+++ src/Bean/index.js
@@ -0,0 +1,59 @@
+/*
+	# @stews/bean #
+	:: created by @paishee/@nuttmegg using ::
+	- aepl: https://github.com/paishee/aepl
+  
+	## LINKS ##
+	- https://github.com/paishee/stews/tree/main/src/Bean
+	- https://npmjs.com/package/@stews/bean
+
+ 	## MAIN ##
+	- https://github.com/paishee/stews
+	- https://npmjs.com/package/stews
+
+ 	## PARTS ##
+   	- https://npmjs.com/package/@stews/soup
+	- https://npmjs.com/package/@stews/stew
+ 	- https://npmjs.com/package/@stews/noodle
+  	- https://npmjs.com/package/@stews/random
+
+*/
+
+
+// imports
+const cl = require('aepl');
+const fs = require('fs');
+
+
+
+// main class
+cl.init("Bean", class {
+    constructor(object) {
+
+		// constructs
+		const __form = require('./construct/__form.js');
+		const __proxy = require('./construct/__proxy/index.js');
+
+				
+		// formatting construct
+		__form.bind(this)(object, joiner);
+
+
+		// creates the proxy
+        return new Proxy(this, __proxy);
+	}
+});
+
+
+
+// export
+module.exports = Bean;
+
+
+
+// compilers
+const compile = require('./compile');
+compile('/builders');
+compile('/functions');
+compile('/properties');
+compile('/internals');
