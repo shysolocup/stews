5b12759d98a155cbcf6f567cc5236b612833628b
paige <88659700+paishee@users.noreply.github.com>
2024-02-07 11:20:37 -0600

    Create chug.js

---
+++ src/Bean/functions/chug.js
@@ -0,0 +1,12 @@
+const Bean = require('../index.js');
+const Soup = require('@stews/soup');
+
+
+function BeanChug(func) {
+	let stuff = Soup.from(this.copy().toString(), "");
+	return stuff.chug(func);
+}
+
+
+Bean.newF("chug", BeanChug);
+Bean.newF("every", BeanChug);
