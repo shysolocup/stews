800196dcdbe67edec7f4ee3c9770ade29d527cec
paige <88659700+paishee@users.noreply.github.com>
2024-02-07 12:10:58 -0600

    Update equals.js

--- src/Bean/functions/equals.js
+++ src/Bean/functions/equals.js
@@ -3,7 +3,7 @@ const Bean = require('../index.js');
 
 function BeanEquals(...args) {
         if (args[0] instanceof Array) args = args[0];
-        let stuff = this.ints;
+        let stuff = this.ints.flat();
 
         for (let i = 0; i < args.length; i++) {
             if (stuff == args[i]) return true;
