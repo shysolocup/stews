cc65320119dc842a6cba30a9103116d69d0ec4f1
paige <88659700+paishee@users.noreply.github.com>
2024-02-07 13:49:25 -0600

    Update map.js

--- src/Bean/functions/map.js
+++ src/Bean/functions/map.js
@@ -6,20 +6,17 @@ function BeanMap(func) {
     let copy = this.copy();
     
 	if (copy.ints[0] instanceof Array) {
-		stuff = new Soup( copy.ints[0].map(func) )
-		stuff2 = this.ints[1].forEach( (v, i) => {
-	        if (i+(this.ints[0].length) == index) stuff2.push(set_to);
-	        stuff2.push(v);
-		});
+		stuff = new Soup( copy.ints[0].map(func) );
+		stuff2 = new Soup( copy.ints[1].map( (v, i) => func(v, i+(copy.ints[0].length)) ));
 		
-		this.content = parseFloat(`${stuff.join("")}.${stuff2.join("")}`);
+		copy.content = parseFloat(`${stuff.join("")}.${stuff2.join("")}`);
 	}
 	else {
-        stuff = new Soup( this.ints.map(func) );
-		this.content = parseFloat(stuff.join(""));
+        stuff = new Soup( copy.ints.map(func) );
+		copy.content = parseFloat(stuff.join(""));
 	}
 
-    return this;
+    return copy;
 }
 
 
