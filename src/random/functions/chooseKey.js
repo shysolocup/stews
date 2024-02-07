16a8f013bb6cc972ede98f4ee66e4e2ffc0e5199
paige <88659700+paishee@users.noreply.github.com>
2024-02-07 08:23:00 -0600

    Create chooseKey.js

---
+++ src/random/functions/chooseKey.js
@@ -0,0 +1,36 @@
+const random = require('../index.js');
+const Soup = require('@stews/soup');
+const Noodle = require('@stews/noodle');
+
+
+function RandomKeyChoice(object=null) {
+    if (!object && this.binder) object = this.binder;
+    
+    // if object is a string
+    // split then randomly choose
+    if (typeof object == "string") {
+        return object.split(splitter)[Math.floor(Math.random() * (Number(object.split(splitter).length)))];
+    }
+
+
+    // if it's object is an object
+    // turns it into a soup and then randomly chooses
+    else if (typeof object == "object" && !(object instanceof Noodle)) {
+        object = Soup.from(object);
+        let index = Math.floor(Math.random() * (object.length));
+        
+        return (object.type == "list") ? 
+            object.get(index) :
+            object.keys[index]
+    }
+
+
+    // if object is a noodle
+    // gets a random character
+    else if (object instanceof Noodle) {
+        return object.get(Math.floor(Math.random() * (object.length)));
+    }
+}
+
+
+random.newF("chooseKey", RandomKeyChoice);
