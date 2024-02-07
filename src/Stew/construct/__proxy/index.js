e8520c4a2bfa1f4bca3784b93c7f86a1447839ee
paige <88659700+paishee@users.noreply.github.com>
2024-02-07 10:40:49 -0600

    Update index.js

--- src/Stew/construct/__proxy/index.js
+++ src/Stew/construct/__proxy/index.js
@@ -1,3 +1,4 @@
+
 const fs = require('fs');
 
 
@@ -5,7 +6,7 @@ module.exports = {};
 
 
 // compiling properties
-let prox_dir = require('./data/_funkydir');
+let prox_dir = `${__dirname}/data`
 let prox = fs.readdirSync(prox_dir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));  
 prox.forEach( (file) => {
 	require(`./data/${file}`);
