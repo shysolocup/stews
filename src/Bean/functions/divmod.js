9f03adb5ea9d4f5743cf74c09f15a42826c95040
paige <88659700+paishee@users.noreply.github.com>
2024-02-08 09:44:31 -0600

    Update divmod.js

--- src/Bean/functions/divmod.js
+++ src/Bean/functions/divmod.js
@@ -6,7 +6,7 @@ const Bean = require('../index.js');
 function BeanDivmod(y) {
     let x = this.content;
     let integer = Math.floor(x / y);
-    let value = Math.foor(x % y);
+    let value = Math.floor(x % y);
     return [integer, value];
 }
 
